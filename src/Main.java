85294a04acfbd85077ce73958cde9606d165c119
Jatin Srivastava <141351678+JxTIN21@users.noreply.github.com>
2024-05-05 00:05:08 +0530

    Java program to return the indices of two number such that they add up to the target.

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+//Given an array of integers numbs and an integer target, return indices of the two numbers such that they add up to target.
+//   You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order.
+public class Main {
+    public static int[] twoSum(int[] numbs, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbs.length; i++) {
+            int curr = numbs[i];
+            int x = target - curr;
+            if (map.containsKey(x)) {
+                return new int[] {map.get(x), i};
+            }
+            map.put(curr, i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] numbs = {3, 6, 9, 4, 2};
+        int target = 11;
+        int[] result = twoSum(numbs, target);
+        System.out.println(result[0] + "," + result[1]);
+    }
+}
